0902e7f8863f8b94280fbd3fa7931b4175fd121a
Samia Hamed <samia.hamed@gmail.com>
2022-08-10 21:46:16 -0700

    find paths

--- why/graph.py
+++ why/graph.py
@@ -1,5 +1,6 @@
 # HOMEWORK: graph traversal algorithms!
 # path_exists(a, b) returns TRUE if you can get to b from a along edges
+
 def unfold_edges(fold):
     edges = []
     for source in fold:
@@ -30,6 +31,34 @@ class Graph():
         # self is a container. a dict is a container. you can add arbitrary attributes into self like you
         # can add keys in a dict
         self.fold = fold_edges(self.edges)
+        self.values = {
+            node: None
+            for node in nodes}
+
+    def get_value(self, key):
+        return self.values[key]
+
+    def set_value(self, key, value):
+        self.values[key] = value
+        
+    def find_paths(self, start, end, visited=None):
+        visited = visited or {}
+        visited[start] = True
+        if start == end:
+            return [(start,)]
+        targets = self.fold.get(start, [])
+        paths = []
+        for target in targets:
+            if target == end:
+                paths.append((start, end))
+            elif target not in visited:
+                target_paths = self.find_paths(target, end, visited)
+                if not len(target_paths) == 0:
+                    paths.extend([
+                        (start,) + path
+                        for path in target_paths])
+        return paths
+        
         
 
 def test_graph():
@@ -52,7 +81,18 @@ def test_graph():
     print(graph.edges)
     print(graph.fold)
     print(unfold)
-    
+
+    graph.set_value('M41', 1000)
+    graph.set_value('milky way', 3489347)
+    graph.set_value('stillness', 8)
+    graph.set_value('andromeda', 13131313)
+    graph.set_value('spork', 71)
+    graph.set_value('dirt', 19)
+    graph.set_value('flap', 99999)
+
+    print(graph.values)
+    print(graph.get_value('andromeda'))
+    print(graph.find_paths('stillness', 'andromeda'))
 
 if __name__ == '__main__':
     test_graph()
